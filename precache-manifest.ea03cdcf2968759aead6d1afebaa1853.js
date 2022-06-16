95818889df618c2c979fee3e4fba99c97c4a3da2
Alex De Sousa <alexndesousa@hotmail.co.uk>
2022-06-16 16:17:37 +0100

    Updates

---
+++ precache-manifest.ea03cdcf2968759aead6d1afebaa1853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac112e4543db1b28f42d7734e33409e",
+    "url": "/artist-search/index.html"
+  },
+  {
+    "revision": "9d16a29f8ebe368da51e",
+    "url": "/artist-search/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "c80a1422664198c0628f",
+    "url": "/artist-search/static/js/2.d1a443bf.chunk.js"
+  },
+  {
+    "revision": "9d16a29f8ebe368da51e",
+    "url": "/artist-search/static/js/main.f5911f15.chunk.js"
+  },
+  {
+    "revision": "03679c46aa7b7099892f",
+    "url": "/artist-search/static/js/runtime-main.b68ccac7.js"
+  }
+]);
